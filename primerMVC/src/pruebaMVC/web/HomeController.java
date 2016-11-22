8875639b16c2d492031820738dacefeada5fbf70
Alberto Garcia de Cortazar Rouret <mum@10.165.1.24>
2016-11-22 17:53:59 +0100

    subida de primer MVC

---
+++ primerMVC/src/pruebaMVC/web/HomeController.java
@@ -0,0 +1,18 @@
+package pruebaMVC.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.ui.ModelMap;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String index(ModelMap model){
+        model.addAttribute("message", "Spring MVC Java Config Example");
+        return "index";
+    }
+
+}
